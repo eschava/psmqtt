ec175508716abae6c6416d99003455d8e1b3b57b
f18m <francesco.montorsi@gmail.com>
2025-04-27 01:22:05 +0200

    f

--- src/psmqtt/main.py
+++ src/psmqtt/main.py
@@ -3,14 +3,11 @@
 # Copyright (c) 2016 psmqtt project
 # Licensed under the MIT License.  See LICENSE file in the project root for full license information.
 #
-# This is the entrypoint of the project:
-# 1. Reads configuration pointed to by PSMQTTCONFIG env var, or use `psmqtt.conf`
-#    by default.
-# 2. Extracts from config file settings, e.g. mqtt broker and schedule.
-# 2. Executes schedule...
-# 3. Performs tasks from the schedule, which involves reading sensors and sending
-#    values to the broker
+# This is the entrypoint of the project which just allocates the PsmqttApp class 
+# and calls its setup() and run() methods. It also exits with the right exit code.
 #
+# Note that the main() function of this module is referenced by pyproject.toml and used
+# to build the Python wheel package
 
 from .psmqtt_app import PsmqttApp
 import sys
