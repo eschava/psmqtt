1e6b18807ef4f90ac120d4e86cf5b6f72f913c3c
f18m <francesco.montorsi@gmail.com>
2025-08-27 17:58:13 +0200

    add more formatters

--- src/psmqtt/formatter.py
+++ src/psmqtt/formatter.py
@@ -17,7 +17,9 @@ default_num_decimal_digits = 2
 # Please note that dividing by 1000 produces Kilobytes, Megabytes, Gigabytes, etc.
 # This is not the same as dividing by 1024, which would produce Kibibytes, Mebibytes, Gibibytes, etc.
 # The former is the SI standard, the latter is the IEC standard.
+# We provide both type of formatters:
 
+# SI standard:
 def jinja2_filter_kb(value:float) -> str:
     return str(value // 1000)
 
@@ -27,9 +29,20 @@ def jinja2_filter_mb(value:float) -> str:
 def jinja2_filter_gb(value:float) -> str:
     return str(value // 1000 // 1000 // 1000)
 
+# IEC standard:
+def jinja2_filter_kib(value:float) -> str:
+    return str(value // 1024)  # kibibyte
+
+def jinja2_filter_mib(value:float) -> str:
+    return str(value // 1024 // 1024)  # mebibyte
+
+def jinja2_filter_gib(value:float) -> str:
+    return str(value // 1024 // 1024 // 1024)  # gibibyte
+
 # the _fractional variants return the value with a default number of 2 fractional digits
 # (customizable providing a filter argument)
 
+# SI standard:
 def jinja2_filter_kb_fractional(value:float, num_decimal_digits=default_num_decimal_digits) -> str:
     return str(round(value / 1000, num_decimal_digits))
 
@@ -38,6 +51,16 @@ def jinja2_filter_mb_fractional(value:float, num_decimal_digits=default_num_deci
 
 def jinja2_filter_gb_fractional(value:float, num_decimal_digits=default_num_decimal_digits) -> str:
     return str(round(value / (1000 * 1000 * 1000), num_decimal_digits))
+
+# IEC standard:
+def jinja2_filter_kib_fractional(value:float, num_decimal_digits=default_num_decimal_digits) -> str:
+    return str(round(value / 1024, num_decimal_digits))
+
+def jinja2_filter_mib_fractional(value:float, num_decimal_digits=default_num_decimal_digits) -> str:
+    return str(round(value / (1024 * 1024), num_decimal_digits))
+
+def jinja2_filter_gib_fractional(value:float, num_decimal_digits=default_num_decimal_digits) -> str:
+    return str(round(value / (1024 * 1024 * 1024), num_decimal_digits))
 
 
 def jinja2_filter_uptime_str(linux_epoch_sec:float) -> str:
@@ -70,12 +93,21 @@ def jinja2_filter_iso8601_str(linux_epoch_sec: float) -> str:
 
 def register_jinja2_filters() -> Environment:
     env = Environment()
+    # SI standard
     env.filters['KB'] = jinja2_filter_kb
     env.filters['MB'] = jinja2_filter_mb
     env.filters['GB'] = jinja2_filter_gb
     env.filters['KB_fractional'] = jinja2_filter_kb_fractional
     env.filters['MB_fractional'] = jinja2_filter_mb_fractional
     env.filters['GB_fractional'] = jinja2_filter_gb_fractional
+    # IEC standard
+    env.filters['KiB'] = jinja2_filter_kib
+    env.filters['MiB'] = jinja2_filter_mib
+    env.filters['GiB'] = jinja2_filter_gib
+    env.filters['KiB_fractional'] = jinja2_filter_kib_fractional
+    env.filters['MiB_fractional'] = jinja2_filter_mib_fractional
+    env.filters['GiB_fractional'] = jinja2_filter_gib_fractional
+
     env.filters['uptime_str'] = jinja2_filter_uptime_str
     env.filters['uptime_sec'] = jinja2_filter_uptime_sec
     env.filters['iso8601_str'] = jinja2_filter_iso8601_str
